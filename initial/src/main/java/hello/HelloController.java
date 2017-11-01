a269205b99ba664f840edc94dcda78aa9a56a75e
Mitch-Eaton <mitchell.eaton@maine.edu>
2017-11-01 00:27:14 -0400

    Finally wow

--- initial/src/main/java/hello/HelloController.java
+++ initial/src/main/java/hello/HelloController.java
@@ -1,14 +1,43 @@
 package hello;
 
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
 
-@RestController
-public class HelloController {
-    
+import javax.inject.Inject;
+
+@SpringBootApplication
+@Controller
+public class HelloController
+{
+    private GreetingService greetingService;
+
+    @ResponseBody
     @RequestMapping("/")
-    public String index() {
-        return "Greetings from Spring Boot!";
+    public String helloWorld()
+    {
+        return "Hello, World!";
+    }
+
+    @ResponseBody
+    @RequestMapping(value = "/", params = {"name"})
+    public String helloName(@RequestParam("name") String name)
+    {
+        return this.greetingService.getGreeting(name);
+    }
+
+    @Inject
+    public void setGreetingService(GreetingService greetingService)
+    {
+        this.greetingService = greetingService;
     }
     
+    
+    //hack discussed w/ Nick to avoid noSuchMethodError
+    public static void main(String[] args) {
+        SpringApplication.run(HelloController.class, args);
+    }
 }
